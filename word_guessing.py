704dbd75670090a667648821e13dccf390b446d4
gisingizo1 <gisingizog@gmail.com>
2024-02-06 14:28:54 +0200

    Added guess handling operation

--- word_guessing.py
+++ word_guessing.py
@@ -1,7 +1,32 @@
+import random
 fruits = ['Mango','Pineapple','Watermelon','Dragonfruit','Fig','Starfruit','Rambutan','Durian','Cherimoya','Passionfruit'];
 
 cities = ['Tokyo','Berlin','Santorini','Rio de Janeiro','Kigali','Nairobi'];
 
 print('Welcome to the word guessing game!!!')
-print('Choose the category of words to guess from: \n1. Fruits\n 2. Cities');
+choice = int(input('Choose the category of words to guess from: \n1. Fruits\n2. Cities\n\nAnswer:  '));
+
+if(choice == 1):
+    random_word = random.choice(fruits);
+elif choice == 2:
+    random_word = random.choice(cities);
+else : 
+    print('Invalid Response');
+
+tries = len(random_word);
+fails = 0;
+guesses = '';
+while tries > 0:
+    print(f'Remaining tries: {tries}')
+    guess = input('Enter the letter: ');
+    guesses += guess;
+    tries -= 1;
+    for char in random_word:
+        if char in guesses:
+            print(char, end=' ');
+        else:
+            print('_',end=' ');
+            fails += 1;
+
+    
 
